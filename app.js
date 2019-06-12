b4050d3a246d838ed39b068f6791bf290e44b5ca
Michael Mueller <michael17mueller@gmail.com>
2019-06-12 14:59:47 -0400

    create blur and brightness effect filter for image upon content expansion

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const contentBox = document.querySelector('.content-box');
 
 contentBox.addEventListener('click', () => {
   contentBox.classList.toggle('box-scaler');
+  document.querySelector('.flower-image').classList.toggle('blur-filter');
 })
